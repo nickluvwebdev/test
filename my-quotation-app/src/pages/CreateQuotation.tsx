e999260f6748cc429a074d5c42b29bd37d33111c
nickluvwebdev <Patcharapol.Luk@student.mahidol.edu>
2025-10-31 01:46:08 +0700

    test

---
+++ my-quotation-app/src/pages/CreateQuotation.tsx
@@ -0,0 +1,3 @@
+export default function CreateQuotation() {
+  return <h1 className="text-2xl font-bold text-blue-600">Create Quotation Page</h1>;
+}
---
+++ my-quotation-app/src/pages/Quotation.tsx
@@ -0,0 +1,3 @@
+export default function Quotation() {
+  return <h1 className="text-2xl font-bold text-blue-600">Quotation Page</h1>;
+}
